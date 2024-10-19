61b2d45b1186e69f4b15b8aeeede3343883b5cc2
Matthias Kalms <mtkalms@gmail.com>
2024-10-19 10:31:33 -0700

    Update layout
    - Ensure page is height of window

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import Terminal from '@/components/Terminal'
 
 
 export default function Home() {
-  return <div className="relative mx-auto mt-64 max-w-3xl">
-    <Terminal lines={[
-      {path: ["mtkalms.github.io"], content: "git clone git@github.com:mtkalms/portfolio.git && cd portfolio"},
-      {path: ["mtkalms.github.io", "portfolio"], branch: "main", status: "clean", content: "git commit -m \"Add placeholder\""},
-      {path: ["mtkalms.github.io", "portfolio"], branch: "main", status: "dirty", content: "git push"},
-      {path: ["mtkalms.github.io", "portfolio"], branch: "main", status: "dirty", content: "Still working on it ..."},
-    ]}/>
+  return <div className="relative mx-auto max-w-3xl">
+    <div className="pt-64">
+      <Terminal lines={[
+        {path: ["mtkalms.github.io"], content: "git clone git@github.com:mtkalms/portfolio.git && cd portfolio"},
+        {path: ["mtkalms.github.io", "portfolio"], branch: "main", status: "clean", content: "git commit -m \"Add placeholder\""},
+        {path: ["mtkalms.github.io", "portfolio"], branch: "main", status: "dirty", content: "git push"},
+        {path: ["mtkalms.github.io", "portfolio"], branch: "main", status: "dirty", content: "Still working on it ..."},
+      ]}/>
+    </div>
   </div>
 }
