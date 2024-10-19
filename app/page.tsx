8777e1f38ebef1c366fa8043bb3b49ad763e4dc8
Matthias Kalms <mtkalms@gmail.com>
2024-10-19 10:27:27 -0700

    Render lines with Terminal component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
-import Image from 'next/image'
-
-import next from '../public/next.svg'
-import vercel from '../public/vercel.svg'
 import Terminal from '@/components/Terminal'
 
 
 export default function Home() {
   return <div className="relative mx-auto mt-64 max-w-3xl">
-    <Terminal/>
+    <Terminal lines={[
+      {path: ["mtkalms.github.io"], content: "git clone git@github.com:mtkalms/portfolio.git && cd portfolio"},
+      {path: ["mtkalms.github.io", "portfolio"], branch: "main", status: "clean", content: "git commit -m \"Add placeholder\""},
+      {path: ["mtkalms.github.io", "portfolio"], branch: "main", status: "dirty", content: "git push"},
+      {path: ["mtkalms.github.io", "portfolio"], branch: "main", status: "dirty", content: "Still working on it ..."},
+    ]}/>
   </div>
 }
--- components/Terminal.tsx
+++ components/Terminal.tsx
@@ -3,26 +3,40 @@ import styles from './Terminal.module.css'
  
 const semiBold = localFont({src: './../fonts/FiraCode/SemiBold.ttf', display: 'swap'})
 
-function Terminal() {
+interface TerminalLine {
+  path: string[];
+  content: string;
+  status?: "dirty" | "clean";
+  branch?: string;
+}
+
+interface TerminalProps {
+  lines: TerminalLine[];
+}
+
+function Terminal({lines}: TerminalProps) {
   return <div className="p-5 text-gray-100 text-sm bg-gray-800 border-x-gray-500 border-y-gray-600 border rounded-xl overflow-hidden">
-      <div className="top mb-2 pb-2 flex">
-          <div className="h-3 w-3 bg-red-500 rounded-full"/>
-          <div className="ml-2 h-3 w-3 bg-orange-400 rounded-full"/>
-          <div className="ml-2 h-3 w-3 bg-green-500 rounded-full"/>
-      </div>
-      <div className="mt-4 pb-40 flex text-xs">
-        <div className={semiBold.className}>
-          <span className={[styles.path, styles.dirty].join(' ')}>
-            <span className={styles.step}>mtkalms.github.io</span>
-            <span className={styles.step}>portfolio</span>
-            <wbr/>
-            <span className={styles.step}> main</span>
-          </span>
-          <wbr/>
-          <span className="px-2">Working on it ...</span>
-          <span className="animate-ping">█</span>
+    <div className="top mb-2 pb-2 flex">
+        <div className="h-3 w-3 bg-red-500 rounded-full"/>
+        <div className="ml-2 h-3 w-3 bg-orange-400 rounded-full"/>
+        <div className="ml-2 h-3 w-3 bg-green-500 rounded-full"/>
+    </div>
+    <div className={`mt-4 pb-40 text-xs ${semiBold.className}`}>
+      {lines.map((line, idxLine) => 
+        <div className={`w-full ${styles.line}`} key={`line${idxLine}`}>
+          <span className={[styles.path, line.status && styles[line.status], line.branch && styles.branch].join(' ')}>
+            {line.path.map((step, idxStep) => 
+              <span key={`line${idxLine}-step${idxStep}`} 
+                className={styles.step}>
+                {step}
+              </span>
+            )}
+            {line.branch && <span className={styles.step}> {line.branch}</span>}
+          </span><wbr/>
+          <span className="px-2">{line.content}</span>
         </div>
-      </div>
+      )}
+    </div>
   </div>
 }
 
