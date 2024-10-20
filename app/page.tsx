b2da8e6cf2322b879859e6522202233233e9a39d
Matthias Kalms <mtkalms@gmail.com>
2024-10-20 09:32:46 -0700

    Make process more believable

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,10 @@ export default function Home() {
   return <div className="relative mx-auto max-w-3xl">
     <div className="pt-64">
       <Terminal title="mtkalms@Matthias-MacBook-Pro:~/Repositories/portfolio" lines={[
-        {path: ["mtkalms.github.io"], content: "git clone git@github.com:mtkalms/portfolio.git && cd portfolio"},
-        {path: ["mtkalms.github.io", "portfolio"], branch: "main", status: "clean", content: "git commit -m \"Add placeholder\""},
-        {path: ["mtkalms.github.io", "portfolio"], branch: "main", status: "dirty", content: "git push"},
-        {path: ["mtkalms.github.io", "portfolio"], branch: "main", status: "dirty", content: "Still working on it ..."},
+        {path: ["mtkalms", "Repositories"], content: "git clone git@github.com:mtkalms/portfolio.git && cd portfolio"},
+        {path: ["mtkalms", "portfolio"], branch: "main", status: "clean", content: "git commit -m \"Add placeholder\""},
+        {path: ["mtkalms", "portfolio"], branch: "main", status: "dirty", content: "git push"},
+        {path: ["mtkalms", "portfolio"], branch: "main", status: "dirty", content: "echo \"Still working on it ...\""},
       ]}/>
     </div>
   </div>
