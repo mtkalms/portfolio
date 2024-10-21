8dd1506b58174d93318e7f713bb7ef875c98ccd0
Matthias Kalms <mtkalms@gmail.com>
2024-10-20 23:21:30 -0700

    Fix div overlapping toggle button on mobile

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,15 @@ import Terminal from '@/components/Terminal'
 
 export default function Home() {
   return <div className="relative mx-auto max-w-3xl">
-    <div className="pt-64">
-      <Terminal 
-        className="h-80"
-        title="mtkalms@Matthias-MacBook-Pro:~/Repositories/portfolio" 
-        lines={[
-          {path: ["mtkalms", "Repositories"], content: "git clone git@github.com:mtkalms/portfolio.git && cd portfolio"},
-          {path: ["mtkalms", "portfolio"], branch: "main", status: "clean", content: "git commit -m \"Add placeholder\""},
-          {path: ["mtkalms", "portfolio"], branch: "main", status: "dirty", content: "git push"},
-          {path: ["mtkalms", "portfolio"], branch: "main", status: "dirty", content: "echo \"Still working on it ...\""},
-        ]
+    <Terminal 
+      className="mt-40 h-80"
+      title="mtkalms@Matthias-MacBook-Pro:~/Repositories/portfolio" 
+      lines={[
+        {path: ["mtkalms", "Repositories"], content: "git clone git@github.com:mtkalms/portfolio.git && cd portfolio"},
+        {path: ["mtkalms", "portfolio"], branch: "main", status: "clean", content: "git commit -m \"Add placeholder\""},
+        {path: ["mtkalms", "portfolio"], branch: "main", status: "dirty", content: "git push"},
+        {path: ["mtkalms", "portfolio"], branch: "main", status: "dirty", content: "echo \"Still working on it ...\""},
+      ]
       }/>
-    </div>
   </div>
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,7 @@ interface NavbarProps extends DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTM
 }
 
 function Navbar({children, className, ...props}: NavbarProps) {
-  return <nav className={`absolute w-full top-0 left-0 ${className}`} {...props}>
+  return <nav className={`w-full top-0 left-0 ${className}`} {...props}>
     <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
       <div className="flex gap-4 text-black dark:text-white font-semibold">
         {children}
