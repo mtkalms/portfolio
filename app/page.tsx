c7a679507baf7572b7d3b5c4d1dc99a073bdedeb
Matthias Kalms <mtkalms@gmail.com>
2024-10-28 10:01:14 -0700

    Fix size of Terminal on mobile

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,11 @@ import Terminal from '@/components/Terminal'
 
 export default function Home() {
   return <Terminal 
-    className="h-80 m-auto"
+    className="h-80 m-auto sm:max-w-4xl max-w-xs"
     title="mtkalms@Matthias-MacBook-Pro:~/Repositories/portfolio" 
     lines={[
-      {path: ["mtkalms", "Repositories"], content: "git clone git@github.com:mtkalms/portfolio.git && cd portfolio"},
+      {path: ["mtkalms", "Repositories"], content: "git clone git@github.com:mtkalms/portfolio.git"},
+      {path: ["mtkalms", "Repositories"], content: "cd portfolio"},
       {path: ["mtkalms", "portfolio"], branch: "main", status: "clean", content: "git commit -m \"Add placeholder\""},
       {path: ["mtkalms", "portfolio"], branch: "main", status: "dirty", content: "git push"},
       {path: ["mtkalms", "portfolio"], branch: "main", status: "dirty", content: "echo \"Still working on it ...\""},
