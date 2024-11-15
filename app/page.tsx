9bfe009353ad73599bdbd17b04f8f4f21c267a61
Matthias Kalms <mtkalms@gmail.com>
2024-11-14 23:39:44 -0800

    Remove escape characters

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
     onMaximize={() => setMaximized((state) => !state)}>
       <Terminal.Line path="mtkalms/Repositories">git clone git@github.com:mtkalms/portfolio.git</Terminal.Line>
       <Terminal.Line path="mtkalms/Repositories">cd portfolio</Terminal.Line>
-      <Terminal.Line path="mtkalms/portfolio" branch="main" status="clean">git commit -m \"Add placeholder\"</Terminal.Line>
+      <Terminal.Line path="mtkalms/portfolio" branch="main" status="clean">git commit -m "Add placeholder"</Terminal.Line>
       <Terminal.Line path="mtkalms/portfolio" branch="main" status="dirty">git push</Terminal.Line>
       <Terminal.Line path="mtkalms/portfolio" branch="main" status="dirty">echo "Still working on it ..."</Terminal.Line>
       <Terminal.Line path="mtkalms/portfolio" branch="main" status="dirty">Still working on it ...</Terminal.Line>
